e8fb45a33a76a2636c2cb49440811de7af04008e
tjthechemist <mr.tawatchai@live.com>
2025-10-27 12:33:57 +0700

    fix background path

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,7 +2,7 @@ export const Home = () => {
     return (
         <section>
             <h1 className="font-semibold text-2xl p-4">TJ the Chemists's Personal Website</h1>
-            <div className="bg-[url(Background.png)] relative grid grid-cols-2 justify-center rounded-xl mx-4 shadow-xl">
+            <div className="bg-[url(/Background.png)] relative grid grid-cols-2 justify-center rounded-xl mx-4 shadow-xl">
                 <div>
                     <img className="" src="/profiles.png" alt="Personal Profile"/>
                 </div>
